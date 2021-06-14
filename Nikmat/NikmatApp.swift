f4bccdfa76aeaa58d661e3ff280e45483d596397
Daniel Aditya Istyana <daniel.istyana@tokopedia.com>
2021-06-14 23:56:40 +0700

    Initial Commit

---
+++ Nikmat/NikmatApp.swift
@@ -0,0 +1,17 @@
+//
+//  NikmatApp.swift
+//  Nikmat
+//
+//  Created by Daniel Aditya Istyana on 14/06/21.
+//
+
+import SwiftUI
+
+@main
+struct NikmatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
